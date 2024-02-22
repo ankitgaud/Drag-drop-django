308db69eca92ecc5595f328e06505613044bc761
pvalle6 <pvalle6@lsu.edu>
2024-02-21 22:45:42 -0600

    fixed broken import

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
+from django.conf.urls import include
 
 
 urlpatterns = [
